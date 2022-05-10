98e3cf272bdd416d2cc8f4f2eb8f909980449b27
Nadezhda Suler <nadiasuler@gmail.com>
2022-05-10 22:41:45 +0300

    Задача 2

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (number == 5)
+{
+    Console.WriteLine("Четверг");
+}
