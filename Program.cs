f260c3e1e6ed1a7343076256803e0f3ce4fe49d1
Nadezhda Suler <nadiasuler@gmail.com>
2022-05-11 00:24:42 +0300

    Задача 3

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = -a;
+
+while(b <= a)
+{
+    Console.Write(b);
+    b++;
+    if(b<=a)
+    {
+        Console.Write(", ");
+    }
+}
